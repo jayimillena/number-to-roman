c22b3c0b8d89fcd1debb69badec85f78515a5fa8
Two <22344029+jayimillena@users.noreply.github.com>
2023-10-10 11:23:48 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Convert number to Roman Numeral</title>
+</head>
+<body>
+  <form action="process.php" method="post">
+    <label for="Number">Number</label>
+    <input type="text" placeholder="Number here" name="number">
+    <button type="submit">Convert</button>
+  </form>
+</body>
+</html>
---
+++ process.php
@@ -0,0 +1,24 @@
+<?php 
+
+/**
+ * @param int $number
+ * @return string
+ * Credits to user2095686 at StackOverflow
+ */
+
+function numberToRomanRepresentation($number) {
+    $map = array('M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400, 'C' => 100, 'XC' => 90, 'L' => 50, 'XL' => 40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1);
+    $returnValue = '';
+    while ($number > 0) {
+        foreach ($map as $roman => $int) {
+            if($number >= $int) {
+                $number -= $int;
+                $returnValue .= $roman;
+                break;
+            }
+        }
+    }
+    return $returnValue;
+}
+
+echo numberToRomanRepresentation($_POST['number']);
